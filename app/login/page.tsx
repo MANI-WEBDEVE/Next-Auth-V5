08de4e0d38530f3427a1dc2ffe75b8839c3d874c
Muhammad Inam <nudmaufon@gmail.com>
2024-10-18 12:57:55 +0500

    login-and-signup-UI

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,10 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
 const page = () => {
   return (
     <>
@@ -20,28 +23,67 @@ const page = () => {
           <TabsContent value="account">
             <Card>
               <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
+                <CardTitle className="text-3xl font-semibold text-center uppercase">
+                  Login
+                </CardTitle>
+                <CardDescription></CardDescription>
               </CardHeader>
               <CardContent>
-                <p>Card Content</p>
+                <form>
+                  <div className="flex items-center justify-center gap-4 flex-col">
+                    <Input placeholder="Enter Your Email" />
+                    <Input placeholder="Enter Your Password" />
+                  </div>
+                  <div className="mt-3">
+                    <Button className="w-full">Login</Button>
+                  </div>
+                  
+                </form>
+                <div className="mt-2 text-center mb-2 font-medium">OR</div>
+                <form>
+                    <div className="mt-3 flex ">
+                      <Button variant={"outline"} className="w-full bg-blue- font-medium text-md hover:bg-blue-500 transition-all duration-500"><FcGoogle /> Login With Google</Button>
+                    </div>
+                  </form>
               </CardContent>
-              <CardFooter>
-                <p>Card Footer</p>
+              <CardFooter className="flex items-center justify-end text-xs font-medium text-blue-500">
+                <Link className="" href="/signup">
+                  Developed By Inam
+                </Link>
               </CardFooter>
             </Card>
           </TabsContent>
           <TabsContent value="password">
-            <Card>
+          <Card>
               <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
+                <CardTitle className="text-3xl font-semibold text-center uppercase">
+              Sign Up
+                </CardTitle>
+                <CardDescription></CardDescription>
               </CardHeader>
               <CardContent>
-                <p>Card Content</p>
+                <form>
+                  <div className="flex items-center justify-center gap-4 flex-col">
+                    <Input placeholder="Enter Your Name" />
+                    <Input placeholder="Enter Your Email" />
+                    <Input placeholder="Enter Your Password" />
+                  </div>
+                  <div className="mt-3">
+                    <Button className="w-full">Sign Up</Button>
+                  </div>
+                  
+                </form>
+                <div className="mt-2 text-center mb-2 font-medium">OR</div>
+                <form>
+                    <div className="mt-3 flex ">
+                      <Button variant={"outline"} className="w-full bg-blue- font-medium text-md hover:bg-blue-500 transition-all duration-500"><FcGoogle /> Login With Google</Button>
+                    </div>
+                  </form>
               </CardContent>
-              <CardFooter>
-                <p>Card Footer</p>
+              <CardFooter className="flex items-center justify-end text-xs font-medium text-blue-500">
+                <Link className="" href="/signup">
+                  Developed By Inam
+                </Link>
               </CardFooter>
             </Card>
           </TabsContent>
