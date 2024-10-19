853ea40abece443e6f1ef469bc3635b60fa1232f
Muhammad Inam <nudmaufon@gmail.com>
2024-10-19 15:11:20 +0500

    UI-fixed-and-server-action

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,4 @@
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   Card,
@@ -10,8 +11,13 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { FcGoogle } from "react-icons/fc";
+import { FcGoogle } from "react-icons/fc"; 
+import { SignUp } from "@/action/SignUp";
+// import UserModel from "@/Schemas/userModel";
+// // import { useToast } from "@/hooks/use-toast";
+// import { connectDB } from "@/lib/connectDB";
 const page = () => {
+  
   return (
     <>
       <div className="flex h-dvh w-full justify-center items-center">
@@ -37,14 +43,18 @@ const page = () => {
                   <div className="mt-3">
                     <Button className="w-full">Login</Button>
                   </div>
-                  
                 </form>
                 <div className="mt-2 text-center mb-2 font-medium">OR</div>
                 <form>
-                    <div className="mt-3 flex ">
-                      <Button variant={"outline"} className="w-full bg-blue- font-medium text-md hover:bg-blue-500 transition-all duration-500"><FcGoogle /> Login With Google</Button>
-                    </div>
-                  </form>
+                  <div className="mt-3 flex ">
+                    <Button
+                      variant={"outline"}
+                      className="w-full bg-blue- font-medium text-md hover:bg-blue-500 transition-all duration-500"
+                    >
+                      <FcGoogle /> Login With Google
+                    </Button>
+                  </div>
+                </form>
               </CardContent>
               <CardFooter className="flex items-center justify-end text-xs font-medium text-blue-500">
                 <Link className="" href="/signup">
@@ -54,31 +64,34 @@ const page = () => {
             </Card>
           </TabsContent>
           <TabsContent value="password">
-          <Card>
+            <Card>
               <CardHeader>
                 <CardTitle className="text-3xl font-semibold text-center uppercase">
-              Sign Up
+                  Sign Up
                 </CardTitle>
-                <CardDescription></CardDescription>
               </CardHeader>
               <CardContent>
-                <form>
+                <form action={SignUp}>
                   <div className="flex items-center justify-center gap-4 flex-col">
-                    <Input placeholder="Enter Your Name" />
-                    <Input placeholder="Enter Your Email" />
-                    <Input placeholder="Enter Your Password" />
-                  </div>
+                    <Input placeholder="Enter Your Name" name="name" />
+                    <Input placeholder="Enter Your Email" name="email" />
+                    <Input placeholder="Enter Your Password" name="password" />
+                  </div> 
                   <div className="mt-3">
-                    <Button className="w-full">Sign Up</Button>
+                    <Button className="w-full" type="submit">Sign Up</Button>
                   </div>
-                  
                 </form>
                 <div className="mt-2 text-center mb-2 font-medium">OR</div>
                 <form>
-                    <div className="mt-3 flex ">
-                      <Button variant={"outline"} className="w-full bg-blue- font-medium text-md hover:bg-blue-500 transition-all duration-500"><FcGoogle /> Login With Google</Button>
-                    </div>
-                  </form>
+                  <div className="mt-3 flex ">
+                    <Button
+                      variant={"outline"}
+                      className="w-full bg-blue- font-medium text-md hover:bg-blue-500 transition-all duration-500"
+                    >
+                      <FcGoogle /> Login With Google
+                    </Button>
+                  </div>
+                </form>
               </CardContent>
               <CardFooter className="flex items-center justify-end text-xs font-medium text-blue-500">
                 <Link className="" href="/signup">
